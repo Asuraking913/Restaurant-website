925e26f58ff005e92999ba702ea122b812047672
Asura king913 <israelshedrack913@gmail.com>
2023-12-31 21:14:52 +0100

    New changes

--- script.js
+++
@@ -1,6 +0,0 @@
-const imgBtn1 = document.getElementById("img-btn1");
-const imgBtn2 = document.getElementById("img-btn2");
-const imgList = document.querySelectorAll(".fd-list");
-console.log(imgBtn1);
-console.log(imgBtn2);
-console.log(imgList);
